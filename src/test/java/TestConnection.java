f5ad61eec0e9efa4c4e5f5245f03202e6f7c7134
LvSir <1532513022@qq.com>
2022-04-29 13:20:57 +0800

    push commit

--- src/test/java/TestConnection.java
+++ src/test/java/TestConnection.java
@@ -23,5 +23,6 @@ public class TestConnection {
         System.out.println("1");
         System.out.println("2");
         System.out.println("3");
+        System.out.println("push test!");
     }
 }
